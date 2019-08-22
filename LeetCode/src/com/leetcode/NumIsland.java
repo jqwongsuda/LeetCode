fab7464d6fe1cea33f022b3a7ce858deb9c207a6
jqwongsuda@outlook.com <jqwongsuda@outlook.com>
2019-08-22 18:38:46 +0800

    first commit

---
+++ LeetCode/src/com/leetcode/NumIsland.java
@@ -0,0 +1,70 @@
+package com.leetcode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumIsland {
+
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0)
+        {
+            return 0;
+        }
+        int res = 0;
+        Boolean[][] flag = new Boolean[grid.length][grid[0].length];
+        for(Boolean[] flags : flag)
+        {
+            Arrays.fill(flags,false);
+        }
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                if(grid[i][j] != '1' || flag[i][j] == true)
+                    continue;
+                res++;
+                Queue<Integer> queue = new LinkedList();
+                queue.add(i);
+                queue.add(j);
+                flag[i][j] = true;
+
+                while(!queue.isEmpty())
+                {
+                    var x = queue.poll();
+                    var y = queue.poll();
+                    if(x + 1 < grid.length && grid[x+1][y] == '1' && flag[x+1][y] == false)
+                    {
+                        queue.add(x+1);
+                        queue.add(y);
+                        flag[x+1][y] = true;
+                    }
+                    if(x-1>0 && grid[x-1][y] == '1' && flag[x-1][y] == false)
+                    {
+                        queue.add(x-1);
+                        queue.add(y);
+                        flag[x-1][y] = true;
+
+                    }
+                    if(y+1 < grid[0].length && grid[x][y+1] == '1' && flag[x][y+1] == false)
+                    {
+                        queue.add(x);
+                        queue.add(y+1);
+                        flag[x][y+1] = true;
+
+                    }
+                    if(y-1>0 && grid[x][y-1] == '1' && !flag[x][y - 1])
+                    {
+                        queue.add(x);
+                        queue.add(y-1);
+                        flag[x][y-1] = true;
+
+                    }
+
+                }
+            }
+        }
+        return res;
+
+    }
+}
---
+++ LeetCode/test/com/leetcode/numIslandTest.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class numIslandTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void numIslands() {
+        char[][] grid = new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
+        NumIsland tmp = new NumIsland();
+        int res = tmp.numIslands(grid);
+        System.out.println("grid总共有" + res + "个岛屿！");
+        char[][] grid1 = new char[][]{{'1','1','1'},{'0','1','0'},{'1','1','1'}};
+        NumIsland tmp1 = new NumIsland();
+        int res1 = tmp.numIslands(grid1);
+        System.out.println("gred1总共有" + res1 + "个岛屿！");
+    }
+}
